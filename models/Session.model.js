ea315d58180863d27e3354fc9a168c2088854681
MercedesAmorGallart <127596167+MercedesAmorGallart@users.noreply.github.com>
2023-08-31 13:06:11 +0200

    session routes

---
+++ models/Session.model.js
@@ -0,0 +1,71 @@
+const { Schema, model } = require("mongoose");
+
+
+
+const sessionSchema = new Schema({
+
+sessionName: {
+    type: String,
+    required: true
+},
+eventName: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+    required: true
+},
+description: {
+    type: String
+},
+day: {
+    type: Number,
+    required: true
+},
+
+dateSession: {
+    type: String,
+    required: true
+},
+
+startHour: {
+    type: String,
+    required: true
+},
+endTHour: {
+    type: String,
+    required: true
+},
+isAvailable: {
+    type: Boolean,
+    required: true,
+    default: false
+},
+
+hall: {
+    type: String,
+    
+},
+capacityHall: Number,
+hostedBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+},
+assistants: [{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+}]
+})
+
+
+
+
+
+
+
+
+
+
+const Session = model("Session", sessionSchema);
+
+module.exports = Session;
+
+
--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
-const Event = require("../models/Events.model");
+const Event = require("../models/Event.model");
+const Session = require("../models/Session.model");
+
+
+// RUTAS DE LOS EVENTOS
 
 // GET "/api/events" => lista de todos los eventos.
 
@@ -33,7 +37,7 @@ router.post("/", async (req, res, next) => {
   } = req.body;
 
 
-  if ( !eventName || !startDate || !endDate || !itsFree || !capacity || !sector || !description ) {
+  if (!eventName || !startDate || !endDate || !itsFree || !capacity || !sector || !description) {
     res.json("todos los campos deben estar completos")
   }
 
@@ -83,7 +87,7 @@ router.put("/:eventId", async (req, res, next) => {
     description,
   } = req.body;
 
-  if ( !eventName || !startDate || !endDate || !itsFree || !capacity || !sector || !description ) {
+  if (!eventName || !startDate || !endDate || !itsFree || !capacity || !sector || !description) {
     res.json("todos los campos deben estar completos")
   }
 
@@ -112,14 +116,135 @@ router.put("/:eventId", async (req, res, next) => {
 
 router.delete("/:eventId", async (req, res, next) => {
 
-    const eventId = req.params.eventId;
-    try {
-        await Event.findByIdAndDelete(eventId)
-        res.json("Este evento fue eliminado.")
-    } catch (error) {
-        next(error)
-    }
+  const eventId = req.params.eventId;
+  try {
+    await Event.findByIdAndDelete(eventId)
+    await Session.deleteMany({
+      eventName: eventId
+    })
+
+    res.json("Este evento fue eliminado.")
+  } catch (error) {
+    next(error)
+  }
 })
+
+
+// RUTAS DE LAS SESIONES
+
+// GET "/api/events/:eventId/sessions" => lista todos las sesiones de un evento
+router.get("/:eventId/sessions", async (req, res, next) => {
+
+  try {
+    const response = await Session.find({ eventName: req.params.eventId })
+      .populate("eventName")
+    res.json(response)
+    console.log("params", req.params.eventId)
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+// POST "/api/events/:eventId/sessions" => añadir sesiones a un evento
+router.post("/:eventId/sessions", async (req, res, next) => {
+
+  const { sessionName, description, day, dateSession, startHour, endTHour, isAvailable, hall, capacityHall } = req.body
+  
+  if (!sessionName || !description || !day || !dateSession || !startHour || !endTHour || !isAvailable) {
+    res.json("todos los campos deben estar completos")
+  }
+
+  try {
+    const response = await Session.create({
+      sessionName,
+      eventName: req.params.eventId,
+      description,
+      day,
+      dateSession,
+      startHour,
+      endTHour,
+      isAvailable,
+      hall,
+      capacityHall
+
+    })
+
+    res.json(response)
+    console.log("params", req.params.eventId)
+
+  } catch (error) {
+    next(error)
+  }
+
+
+})
+
+// PUT "/api/events/:eventId/sessions/:sessionId" => Editar detalles de una sesión
+
+router.put("/:eventId/sessions/:sessionId", async (req, res, next) => {
+  const {sessionId, eventId}  = req.params
+ 
+  const {
+    sessionName,
+    eventName,
+    description,
+    day,
+    dateSession,
+    startHour,
+    endTHour,
+    isAvailable,
+    hall,
+    capacityHall
+  } = req.body;
+
+  if (!sessionName || !description || !day || !dateSession || !startHour || !endTHour || !isAvailable) {
+    res.json("todos los campos deben estar completos")
+  }
+
+
+  try {
+    await Session.findByIdAndUpdate(sessionId, {
+      sessionName,
+      eventName: eventId,
+      description,
+      day,
+      dateSession,
+      startHour,
+      endTHour,
+      isAvailable,
+      hall,
+      capacityHall
+    });
+
+    res.json("Session modifed");
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+// DELETE "/api/events/:eventId/sessions/:sessionId" => Eliminar una session
+
+router.delete("/:eventId/sessions/:sessionId", async (req, res, next) => {
+
+  const sessionId = req.params.sessionId;
+  try {
+    await Session.findByIdAndDelete(sessionId)
+
+  res.json("Esta sesión fue eliminada.")
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+
+
+
+
+
 
 
 module.exports = router;
